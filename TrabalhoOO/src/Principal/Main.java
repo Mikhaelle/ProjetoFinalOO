fb3ec4dbc449dc1afcf0700d8dacdb7f9560968a
Mikhaelle <mikhabueno@yahoo.com>
2018-06-23 16:23:36 -0300

    limpando códigos da master

--- TrabalhoOO/src/Principal/Main.java
+++ TrabalhoOO/src/Principal/Main.java
@@ -9,17 +9,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// cadastrando republica
-		String nomeRep = JOptionPane.showInputDialog("Qual o nome da republica ?");
-		Republica rep = new Republica(nomeRep);
-
-		JOptionPane.showMessageDialog(null, "O nome da republica é: " + rep.getNome());
 		
-		//rep.cadastroMorador();
-		rep.cadastrarDespesa();
-		//rep.pesquisarCategoriaDespesa();
-		//rep.retirarCategoriaDespesa();
-		rep.valorTotalDespesas();
 	}
 
 }
