09e1c26f41c9385da354f4287ff8ce69200d4080
Marcos Vinícius <38227184+marcos-mv@users.noreply.github.com>
2018-06-30 21:57:04 -0300

    Add files via upload

---
+++ TrabalhoOO/Morador.java
@@ -0,0 +1,20 @@
+
+public class Morador {
+	
+	String nome, email, rendimento;
+	
+	public Morador(String nome, String email, String rendimento) {
+		
+		this.nome=nome;
+		this.email=email;
+		this.rendimento=rendimento;
+		
+	}
+
+	public void adicionarMorador() {
+		System.out.println("Nome: "+nome);
+		System.out.println("Email: "+email);
+		System.out.println("Rendimento: "+rendimento);
+		
+	}
+}
