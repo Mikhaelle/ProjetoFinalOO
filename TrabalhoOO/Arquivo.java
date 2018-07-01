2865ad344c58760241101f514d1140b11f10acbc
Marcos Vinícius <38227184+marcos-mv@users.noreply.github.com>
2018-07-01 10:22:43 -0300

    Update Arquivo.java

--- TrabalhoOO/Arquivo.java
+++ TrabalhoOO/Arquivo.java
@@ -36,6 +36,72 @@ public class Arquivo {
 		}
 	}
 	
+	public void excluir(String nomeE) {
+		try {
+		File f= new File("Moradores.txt");
+		if(f.exists()) {
+			FileReader fr= new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
+			String linha;
+			
+			int numLinhas=0;
+			
+			while((linha=br.readLine())!=null) {
+				
+			numLinhas++;
+			}
+			
+			String contatos [] = new String[numLinhas];
+			br.close();
+			fr.close();
+			br = new BufferedReader(fr = new FileReader(f));
+			
+			int i=0;
+			while((linha=br.readLine())!=null) {
+				
+				contatos[i]= linha;
+				i++;
+			}
+			br.close();
+			fr.close();
+			FileWriter fw= new FileWriter(f);
+			BufferedWriter bw=new BufferedWriter(fw);
+			
+			boolean sinal=false;
+			boolean primeiraLinha = true;
+			
+			for(int j=0; j<contatos.length; j++) {
+				
+				String l []= contatos[j].split(",");
+				if(l[0].equals(nomeE)) {
+					sinal=true;
+					System.out.println("Morador Eliminado!!");
+				}else {
+					if(primeiraLinha == true) {
+					bw.write(contatos[j]);
+					primeiraLinha=false;
+					}else {
+						bw.newLine();
+						bw.write(contatos[j]);
+					}
+				}
+			}
+				if(sinal==false) {
+					System.out.println("Contato não Encontrado!");
+				}
+				bw.close();
+				fw.close();
+				if(numLinhas==1 && sinal==true) {
+					f.delete();
+				}
+		}else {
+			System.out.println("Esse nome não Existe!!");
+		}
+	}catch(Exception e){
+		System.out.println(e);
+	}
+}
+	
 	public void buscar(String nomeB) {
 		try {
 			File f= new File("Moradores.txt");
@@ -60,6 +126,8 @@ public class Arquivo {
 				if(sinal==false) {
 					System.out.println("Nome não Existe!!");
 				}
+				br.close();
+				fr.close();
 			}else {
 				System.out.println("Não existe arquivo para buscar");
 			}
@@ -69,99 +137,3 @@ public class Arquivo {
 			System.out.println(e);
 		}
 	}
-	
-	 public void excluir(String nomeE) {
-		try {
-		File f= new File("Moradores.txt");
-		if(f.exists()) {
-			FileReader fr= new FileReader(f);
-			BufferedReader br = new BufferedReader(fr);
-			String linha;
-			
-			int numLinhas=0;
-			
-			while((linha=br.readLine())!=null) {
-				
-			numLinhas++;
-			}
-			
-			String contatos [] = new String[numLinhas];
-			br.close();
-			fr.close();
-			br = new BufferedReader(new FileReader(f));
-			
-			int i=0;
-			while((linha=br.readLine())!=null) {
-				
-				contatos[i]= linha;
-				i++;
-			}
-			br.close();
-			fr.close();
-			FileWriter fw= new FileWriter(f);
-			BufferedWriter bw=new BufferedWriter(fw);
-			
-			boolean sinal=false;
-			boolean primeiraLinha = true;
-			
-			for(int j=0; j<contatos.length; j++) {
-				
-				String l []= contatos[j].split(",");
-				if(l[0].equals(nomeE)) {
-					System.out.println("Morador Eliminado!!");
-				}else {
-					if(primeiraLinha == true) {
-					bw.write(contatos[j]);
-					primeiraLinha=false;
-					}else {
-						bw.newLine();
-						bw.write(contatos[j]);
-					}
-				}
-				if(sinal==false) {
-					System.out.println("Contato não Encontrado!");
-				}
-				bw.close();
-				fw.close();
-				if(numLinhas==1 && sinal==true) {
-					f.delete();
-				}
-			}
-		}else {
-			System.out.println("Esse nome não Existe!!");
-		}
-	}catch(Exception e){
-		System.out.println(e);
-	}
-}
-
-	public void imprimir() {
-		try {
-			File f=new File("Moradores.txt");
-			if(f.exists()) {
-				FileReader fr = new FileReader(f);
-				BufferedReader br = new BufferedReader(fr);
-				String linha;
-				while((linha = br.readLine()) != null) {
-					
-					//(String nome, String email, String rendimento)
-					
-					String [] contato = linha.split(",");
-					
-					Morador m = new Morador(contato[0],contato[1],contato[2]);
-					
-					m.adicionarMorador();
-					System.out.println("***********************");
-						
-				}
-			}else 
-				{
-				
-				System.out.println("Arquivo não existe");
-				}
-				}catch(Exception e) {
-					System.out.println(e);
-				}
-	}
-}
-		
